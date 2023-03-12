1302ada69959e0d1e38d79844bec527aa966cf41
Ar Kar Aung <arkaraung109@gmail.com>
2023-03-12 14:54:15 +0630

    Complaint Done

--- src/main/java/com/project/complaintmechanism/controller/ComplaintController.java
+++ src/main/java/com/project/complaintmechanism/controller/ComplaintController.java
@@ -8,6 +8,7 @@ import com.project.complaintmechanism.service.CompanyService;
 import com.project.complaintmechanism.service.ComplaintFormService;
 import com.project.complaintmechanism.service.ComplaintTitleService;
 import com.project.complaintmechanism.service.DailyLimitService;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -38,63 +39,138 @@ public class ComplaintController {
     @Autowired
     DailyLimitService dailyLimitService;
 
-    @GetMapping("/")
-    public String showList(Model model) {
+    @GetMapping("/all")
+    public String showAllList(Model model, HttpSession httpSession) {
+
+        httpSession.setAttribute("status", "All");
+        return showList(model, httpSession);
+
+    }
+
+    @GetMapping("/trash")
+    public String showTrashList(Model model, HttpSession httpSession) {
+
+        httpSession.setAttribute("status", "Trash");
+        return showList(model, httpSession);
+
+    }
+
+    @GetMapping("/read")
+    public String showReadList(Model model, HttpSession httpSession) {
+
+        httpSession.setAttribute("status", "Read");
+        return showList(model, httpSession);
+
+    }
+
+    @GetMapping("/unread")
+    public String showUnreadList(Model model, HttpSession httpSession) {
+
+        httpSession.setAttribute("status", "Unread");
+        return showList(model, httpSession);
+
+    }
+
+    @GetMapping("/accepted")
+    public String showAcceptedList(Model model, HttpSession httpSession) {
+
+        httpSession.setAttribute("status", "Accepted");
+        return showList(model, httpSession);
+
+    }
+
+    @GetMapping("/rejected")
+    public String showRejectedList(Model model, HttpSession httpSession) {
+
+        httpSession.setAttribute("status", "Rejected");
+        return showList(model, httpSession);
+
+    }
+
+    @GetMapping("/pending")
+    public String showPendingList(Model model, HttpSession httpSession) {
+
+        httpSession.setAttribute("status", "Pending");
+        return showList(model, httpSession);
+
+    }
+
+    @GetMapping("/solved")
+    public String showSolvedList(Model model, HttpSession httpSession) {
+
+        httpSession.setAttribute("status", "Solved");
+        return showList(model, httpSession);
+
+    }
+
+    @GetMapping("/unsolved")
+    public String showUnsolvedList(Model model, HttpSession httpSession) {
+
+        httpSession.setAttribute("status", "Unsolved");
+        return showList(model, httpSession);
+
+    }
+
+    public String showList(Model model, HttpSession httpSession) {
+
         model.addAttribute("dailyLimit", new DailyLimitModel());
-        return getByPage(model, null, null, null, 1, 5);
+        return getByPage(model, httpSession, null, null, null, 1, 5);
+
     }
 
     @GetMapping("/page")
-    public String getPaginated(Model model, @RequestParam(required = false) String companyName, @RequestParam(required = false) String complaintTitle, @RequestParam(required = false) String date, @RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "5") int size) {
+    public String getPaginated(Model model, HttpSession httpSession, @RequestParam(required = false) String companyName, @RequestParam(required = false) String complaintTitleName, @RequestParam(required = false) String date, @RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "5") int size) {
+
         model.addAttribute("dailyLimit", new DailyLimitModel());
-        return getByPage(model, companyName, complaintTitle, date, page, size);
+        return getByPage(model, httpSession, companyName, complaintTitleName, date, page, size);
 
     }
 
-    public String getByPage(Model model, String companyName, String complaintTitle, String date, int page, int size) {
+    public String getByPage(Model model, HttpSession httpSession, String companyName, String complaintTitleName, String date, int page, int size) {
 
         Pageable paging = PageRequest.of(page - 1, size);
-        Page<ComplaintForm> complaintPage;
+        Page<ComplaintForm> complaintPage = null;
         boolean isEmptyCompanyName = (companyName == null || companyName.equalsIgnoreCase(""));
-        boolean isEmptyComplaintTitle = (complaintTitle == null || complaintTitle.equalsIgnoreCase(""));
+        boolean isEmptyComplaintTitleName = (complaintTitleName == null || complaintTitleName.equalsIgnoreCase(""));
         boolean isEmptyDate = (date == null || date.equalsIgnoreCase(""));
+        String status = (String) httpSession.getAttribute("status");
 
-//        if(isEmptyCompanyName && isEmptyComplaintTitle && isEmptyDate) {
-            complaintPage = complaintFormService.findByPage(paging);
-//        }
-//        else if(!isEmptyCompanyName && isEmptyComplaintTitle && isEmptyDate) {
-//            complaintPage = complaintFormService.findByPageWithCompanyName(companyName, paging);
-//            model.addAttribute("companyName", companyName);
-//        }
-//        else if(isEmptyCompanyName && !isEmptyComplaintTitle && isEmptyDate) {
-//            complaintPage = complaintFormService.findByPageWithComplaintTitle(complaintTitle, paging);
-//            model.addAttribute("complaintTitle", complaintTitle);
-//        }
-//        else if(isEmptyCompanyName && isEmptyComplaintTitle && !isEmptyDate) {
-//            complaintPage = complaintFormService.findByPageWithDate(date, paging);
-//            model.addAttribute("date", date);
-//        }
-//        else if(!isEmptyCompanyName && !isEmptyComplaintTitle && isEmptyDate) {
-//            complaintPage = complaintFormService.findByPageWithCompanyNameAndComplaintTitle(companyName, complaintTitle, paging);
-//            model.addAttribute("companyName", companyName);
-//            model.addAttribute("complaintTitle", complaintTitle);
-//        }
-//        else if(!isEmptyCompanyName && isEmptyComplaintTitle && !isEmptyDate) {
-//            complaintPage = complaintFormService.findByPageWithCompanyNameAndDate(companyName, date, paging);
-//            model.addAttribute("companyName", companyName);
-//            model.addAttribute("date", date);
-//        }
-//        else if(isEmptyCompanyName && !isEmptyComplaintTitle && !isEmptyDate) {
-//            complaintPage = complaintFormService.findByPageWithComplaintTitleAndDate(complaintTitle, date, paging);
-//            model.addAttribute("complaintTitle", complaintTitle);
-//            model.addAttribute("date", date);
-//        }
-//        else {
-//            complaintPage = complaintFormService.findByPageWithCompanyNameAndComplaintTitleAndDate(companyName, complaintTitle, date, paging);
-//            model.addAttribute("companyName", companyName);
-//            model.addAttribute("complaintTitle", complaintTitle);
-//            model.addAttribute("date", date);
-//        }
+        if(isEmptyCompanyName && isEmptyComplaintTitleName && isEmptyDate) {
+            complaintPage = complaintFormService.findByPage(status, paging);
+        }
+        else if(!isEmptyCompanyName && isEmptyComplaintTitleName && isEmptyDate) {
+            complaintPage = complaintFormService.findByPageWithCompanyName(status, companyName, paging);
+            model.addAttribute("companyName", companyName);
+        }
+        else if(isEmptyCompanyName && !isEmptyComplaintTitleName && isEmptyDate) {
+            complaintPage = complaintFormService.findByPageWithComplaintTitleName(status, complaintTitleName, paging);
+            model.addAttribute("complaintTitleName", complaintTitleName);
+        }
+        else if(isEmptyCompanyName && isEmptyComplaintTitleName && !isEmptyDate) {
+            complaintPage = complaintFormService.findByPageWithDate(status, date, paging);
+            model.addAttribute("date", date);
+        }
+        else if(!isEmptyCompanyName && !isEmptyComplaintTitleName && isEmptyDate) {
+            complaintPage = complaintFormService.findByPageWithCompanyNameAndComplaintTitleName(status, companyName, complaintTitleName, paging);
+            model.addAttribute("companyName", companyName);
+            model.addAttribute("complaintTitleName", complaintTitleName);
+        }
+        else if(!isEmptyCompanyName && isEmptyComplaintTitleName && !isEmptyDate) {
+            complaintPage = complaintFormService.findByPageWithCompanyNameAndDate(status, companyName, date, paging);
+            model.addAttribute("companyName", companyName);
+            model.addAttribute("date", date);
+        }
+        else if(isEmptyCompanyName && !isEmptyComplaintTitleName && !isEmptyDate) {
+            complaintPage = complaintFormService.findByPageWithComplaintTitleNameAndDate(status, complaintTitleName, date, paging);
+            model.addAttribute("complaintTitleName", complaintTitleName);
+            model.addAttribute("date", date);
+        }
+        else {
+            complaintPage = complaintFormService.findByPageWithCompanyNameAndComplaintTitleNameAndDate(status, companyName, complaintTitleName, date, paging);
+            model.addAttribute("companyName", companyName);
+            model.addAttribute("complaintTitleName", complaintTitleName);
+            model.addAttribute("date", date);
+        }
 
         List<ComplaintForm> complaintFormList = complaintPage.getContent();
         List<ComplaintModel> complaintList = new ArrayList<>();
@@ -107,7 +183,6 @@ public class ComplaintController {
             List<String> complaintTitleList = new ArrayList<>();
             for(ComplaintTitle ct : complaintTitleSet) {
                 complaintTitleList.add(ct.getName());
-
             }
 
             ComplaintModel complaintModel = ComplaintModel.builder()
@@ -115,7 +190,7 @@ public class ComplaintController {
                                                           .description(description)
                                                           .date(submitted_date)
                                                           .readStatus(complaintForm.isReadStatus() ? "Read" : "Unread")
-                                                          .acceptedStatus(complaintForm.getAcceptedStatus() == -1 ? "Unassigned" : complaintForm.getAcceptedStatus() == 0 ? "Rejected" : "Accepted")
+                                                          .acceptedStatus(complaintForm.getAcceptedStatus() == -1 ? "Pending" : complaintForm.getAcceptedStatus() == 0 ? "Rejected" : "Accepted")
                                                           .solvedStatus(complaintForm.isSolvedStatus() ? "Solved" : "Unsolved")
                                                           .tempDeletedStatus(complaintForm.isTempDeletedStatus())
                                                           .name(complaintForm.getName())
@@ -143,21 +218,39 @@ public class ComplaintController {
     }
 
     @PostMapping("/dailyLimit/update")
-    public String update(@Valid @ModelAttribute("dailyLimit") DailyLimitModel dailyLimitModel, BindingResult result, Model model, RedirectAttributes redirectAttributes) {
+    public String update(@Valid @ModelAttribute("dailyLimit") DailyLimitModel dailyLimitModel, HttpSession httpSession, BindingResult result, Model model, RedirectAttributes redirectAttributes) {
 
         if (!result.hasErrors()) {
             dailyLimitService.save(dailyLimitModel.getMaxLimit());
             redirectAttributes.addFlashAttribute("updated_success", true);
             return "redirect:/api/complaint/";
         }
-        return getByPage(model, null, null, null, 1, 5);
+        return getByPage(model,httpSession, null, null, null, 1, 5);
 
     }
 
     @PostMapping("/changeReadStatus")
     @ResponseBody
     public void changeReadStatus(@RequestParam("id") int id, @RequestParam("status") boolean status) {
+
         complaintFormService.changeReadStatus(id, status);
+
+    }
+
+    @PostMapping("/changeTempDeletedStatus")
+    @ResponseBody
+    public void changeTempDeletedStatus(@RequestParam("id") int id, @RequestParam("status") boolean status) {
+
+        complaintFormService.changeTempDeletedStatus(id, status);
+
+    }
+
+    @PostMapping("/emptyTrash")
+    @ResponseBody
+    public void emptyTrash() {
+
+        complaintFormService.emptyTrash();
+
     }
 
 }
--- src/main/java/com/project/complaintmechanism/service/ComplaintFormService.java
+++ src/main/java/com/project/complaintmechanism/service/ComplaintFormService.java
@@ -13,10 +13,28 @@ public interface ComplaintFormService {
 
     boolean checkLimitExceeded(int dailyLimit);
 
-    Page<ComplaintForm> findByPage(Pageable paging);
+    Page<ComplaintForm> findByPage(String status, Pageable paging);
+
+    Page<ComplaintForm> findByPageWithCompanyName(String status, String companyName, Pageable paging);
+
+    Page<ComplaintForm> findByPageWithComplaintTitleName(String status, String complaintTitleName, Pageable paging);
+
+    Page<ComplaintForm> findByPageWithDate(String status, String date, Pageable paging);
+
+    Page<ComplaintForm> findByPageWithCompanyNameAndComplaintTitleName(String status, String companyName, String complaintTitleName, Pageable paging);
+
+    Page<ComplaintForm> findByPageWithCompanyNameAndDate(String status, String companyName, String date, Pageable paging);
+
+    Page<ComplaintForm> findByPageWithComplaintTitleNameAndDate(String status, String complaintTitleName, String date, Pageable paging);
+
+    Page<ComplaintForm> findByPageWithCompanyNameAndComplaintTitleNameAndDate(String status, String companyName, String complaintTitleName, String date, Pageable paging);
 
     void save(ComplaintFormModel complaintFormModel) throws IOException;
 
-    void changeReadStatus(int id, boolean readStatus);
+    void changeReadStatus(int id, boolean status);
+
+    void changeTempDeletedStatus(int id, boolean status);
+
+    void emptyTrash();
 
 }
